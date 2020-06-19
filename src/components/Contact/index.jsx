b946992ad6b06dc62299d18d9c41a8087c4ab20f
Andrius Martini <andrius.martini1994@gmail.com>
2020-06-19 00:29:41 -0300

    Made contact component responsive

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -10,46 +10,44 @@ const Contact = () => {
     >
       <h1>Contato</h1>
 
-      <div>
-        <form className={ Style.contact_form }>
-          <input
-            data-aos="zoom-out-right"
-            id="name"
-            placeholder="Nome"
-            required={ true }
-            type="text"
-          />
+      <form className={ Style.contact_form }>
+        <input
+          data-aos="zoom-out-right"
+          id="name"
+          placeholder="Nome"
+          required={ true }
+          type="text"
+        />
 
-          <input
-            data-aos="zoom-out-left"
-            id="email"
-            placeholder="E-mail"
-            required={ true }
-            type="email"
-          />
+        <input
+          data-aos="zoom-out-left"
+          id="email"
+          placeholder="E-mail"
+          required={ true }
+          type="email"
+        />
 
-          <input
-            data-aos="zoom-out-right"
-            id="phone"
-            placeholder="Telefone"
-            required={ true }
-            type="text"
-          />
+        <input
+          data-aos="zoom-out-right"
+          id="phone"
+          placeholder="Telefone"
+          required={ true }
+          type="text"
+        />
 
-          <textarea 
-            data-aos="zoom-out-left"
-            id="message"
-            placeholder="Mensagem"
-          />
+        <textarea 
+          data-aos="zoom-out-left"
+          id="message"
+          placeholder="Mensagem"
+        />
 
-          <button 
-            data-aos="zoom-out-right"
-            type="submit"
-          >
-            Enviar
-          </button>
-        </form>
-      </div>
+        <button 
+          data-aos="zoom-out-right"
+          type="submit"
+        >
+          Enviar
+        </button>
+      </form>
     </section>
   )
 }
