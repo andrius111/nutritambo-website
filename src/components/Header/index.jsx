4839264d293bf3f2af8ce29d968a075e73459180
Andrius Martini <andrius.martini1994@gmail.com>
2020-06-18 23:36:36 -0300

    Make the header component responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { FaFacebook, FaInstagram } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaFacebook, FaInstagram, FaBars } from 'react-icons/fa'
 
 import HeaderLogo from '../../assets/header-logo.png'
 import Style from './style.module.scss'
 
 const Header = () => {
+  const [menuBurgerClass, setMenuBurgerClass] = useState(Style.nav_container_responsive + ' ' + Style.hidden)
+  const [headerContainerClass, setHeaderContainerClass] = useState(Style.header_container)
+
+  const handleBurgerMenu = () => {
+    if (menuBurgerClass === Style.nav_container_responsive) {
+      setMenuBurgerClass(Style.nav_container_responsive + ' ' + Style.hidden)
+      setHeaderContainerClass(Style.header_container)
+    } else {
+      setMenuBurgerClass(Style.nav_container_responsive)
+      setHeaderContainerClass(Style.header_container + ' ' + Style.header_container_300)
+    }
+  }
+
   return (
-    <header className={ Style.header_container }>
+    <header className={ headerContainerClass }>
       <div className={ Style.header_topbar_container }>
         <div className={ Style.header_topbar_content }>
           <div className={ Style.header_top_bar_contacts }>
-            <div><strong>André:</strong> 54 99917-2275</div>
-            <div><strong>Martini:</strong> 54 99961-6386</div>
+            <div className={ Style.header_top_bar_contacts_item }><strong>André:</strong> 54 99917-2275</div>
+            <div className={ Style.header_top_bar_contacts_item }><strong>Martini:</strong> 54 99961-6386</div>
           </div>
         </div>
       </div>
@@ -54,6 +67,28 @@ const Header = () => {
             onClick={ () => window.open('https://www.instagram.com/nutritambo', '_blank') }
           />
         </div>
+
+        <div className={ Style.burger_menu_icon }>
+          <FaBars
+            cursor="pointer"
+            size={ 30 }
+            onClick={ () => handleBurgerMenu() }
+          />
+        </div>
+      </nav>
+
+      <nav className={ menuBurgerClass }>
+        <a href="#sobre">
+          Sobre
+        </a>
+
+        <a href="#representacoes">
+          Representações
+        </a>
+
+        <a href="#contato">
+          Contato
+        </a>
       </nav>
     </header>
   )
